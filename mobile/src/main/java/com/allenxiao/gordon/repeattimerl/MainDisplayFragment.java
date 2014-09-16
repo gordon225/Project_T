fce54978d85c05dd6cbb929cc01bb0033f426b46
xiaoleqi <jassy.xiaogmail.com>
2014-09-16 12:53:40 -0400

    test 2

--- mobile/src/main/java/com/allenxiao/gordon/repeattimerl/MainDisplayFragment.java
+++ mobile/src/main/java/com/allenxiao/gordon/repeattimerl/MainDisplayFragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  * Use the {@link MainDisplayFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
+ * test
  */
 public class MainDisplayFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
